027848e2507432ab22c034361a8993047fd5d83d
Rekha <rekha@infisign.io>
2024-03-02 17:14:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demoapp';
+  title = 'Look at me!';
+  value = 50;
+  //Add new method to return hello message
+  clickIncBut(){
+this.value = this.value + 1;
+  }
+  clickDecBut(){
+this.value = this.value - 1;
+  }
 }
