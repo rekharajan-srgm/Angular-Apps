0666363f5ad212e8de97d03126b9e36021fd1296
Rekha <rekha@infisign.io>
2024-06-21 21:33:55 +0530

    Get country list from external api for test

--- login-app/src/app/components/homepage/homepage.component.ts
+++ login-app/src/app/components/homepage/homepage.component.ts
@@ -1,17 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CountryResponse } from './CountryResponse';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
-name: any;
-age: any;
-  regLicence(){
+  licForm: FormGroup;
+  name: any;
+  age: any;
+  countries: string[] = [];
+  isLicenceForm: FormGroup;
+  constructor(private router: Router, private fb: FormBuilder, private http: HttpClient) {
+    this.licForm = this.fb.group({
+      gender: ['male'] //default value
+    });
+    this.isLicenceForm = this.fb.group({
+      isLicence: ['yes'],//default value
+      country: ['']
+    })
+  }
+  ngOnInit() {
+    this.getCountryList();
+    console.log("Country List:",this.countries);
 
   }
-  cancelForm(){
 
+
+  getCountryList(){
+      console.log('calling http');
+    this.http.get<CountryResponse>('https://run.mocky.io/v3/cf4b2506-2b59-4114-b59f-f60a9d14aac0')
+      .subscribe(data => {
+          console.log(data);
+          this.countries = data.countries;
+      });
+      return this.countries;
+  }
+
+  regLicence() {
+    console.log("register or updated licence");
+  }
+  cancelForm() {
+    this.router.navigate(['/homepage']);
+    console.log("cancelled licence");
   }
 }
